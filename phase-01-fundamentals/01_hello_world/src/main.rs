ed54c3add0f40c09864eae183ec2e8b61aa1fa4b
dmaax <dudzcool@gmail.com>
2025-06-29 22:49:34 -0300

    feat: Create hello world project

---
+++ phase-01-fundamentals/01_hello_world/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let bananas: i32 = 2;
+    let apples: i32 = 3;
+    let total_fruits: i32 = bananas + apples;
+
+    println!(
+        "I have {} bananas, {} apples, and {} total fruits.",
+        bananas, apples, total_fruits
+    );
+}
